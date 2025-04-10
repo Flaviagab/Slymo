323706ab4fd89c91cfe30c07c3a2f4c5aa097a1f
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2025-04-10 16:39:21 -0300

    Criação de um arquivo separado para o java script, estilização do formulário

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+const dataForm = document.getElementById("contact-form");
+const notification = document.getElementById("notification")
+
+dataForm.addEventListener("submit", function(event){
+    //impedir o envio
+
+    event.preventDefault();
+
+    //obter os campos
+    const name = document.getElementById("name").value;
+
+    const email = document.getElementById("email").value;
+
+    const message = document.getElementById("message").value;
+
+    //validação
+  if(name && email && message){
+    alert("Mensagem enviada!");
+
+    dataForm.reset();
+
+  } else {
+    notification.textContent = "Por favor preencha todos os campos"
+  } 
+})
